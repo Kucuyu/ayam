76eca5af54a7ac208ff6bf86d1a760473938ea51
Kucuyu <kucuyu01@gmail.com>
2024-11-27 00:37:38 +0700

    first commit

---
+++ app/Http/Controllers/KaryawanController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Karyawan;
+
+class KaryawanController extends Controller
+{
+    public function index()
+    {
+        return view('karyawan.index');
+    }
+
+    public function data()
+    {
+        $karyawan = Karyawan::orderBy('id_karyawan', 'desc')->get();
+
+        return datatables()
+            ->of($karyawan)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($karyawan) {
+                return '
+                <div class="btn-group">
+                    <button type="button" onclick="editForm(`'. route('karyawan.update', $karyawan->id_karyawan) .'`)" class="btn btn-xs btn-info btn-flat"><i class="fa fa-pencil"></i></button>
+                    <button type="button" onclick="deleteData(`'. route('karyawan.destroy', $karyawan->id_karyawan) .'`)" class="btn btn-xs btn-danger btn-flat"><i class="fa fa-trash"></i></button>
+                </div>
+                ';
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $karyawan = Karyawan::create($request->all());
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $karyawan = Karyawan::find($id);
+
+        return response()->json($karyawan);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $karyawan = Karyawan::find($id)->update($request->all());
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $karyawan = Karyawan::find($id)->delete();
+
+        return response(null, 204);
+    }
+}
